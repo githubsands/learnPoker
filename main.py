267444422a4053d3874baf4aeee086eb8a09541f
vracek@protonmail.com <vracek@protonmail.com>
2019-08-18 18:30:29 -0600

    beginnings

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import hands
+import rounds
+
+def main():
+    hands.printHands()
+    #hands.printHandOdds()
+    hands.printHandProbabilities()
+    print(rounds.NAME_OF_ROUNDS())
+    
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ rounds.py
@@ -0,0 +1,9 @@
+def NAME_OF_ROUNDS():
+    rounds = {
+    0: "preflop", 
+    1: "flop",
+    2: "turn", 
+    3: "river", 
+    4: "showdown", 
+     }
+    return rounds
